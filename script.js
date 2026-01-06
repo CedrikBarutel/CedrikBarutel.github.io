8f8f44cedfa42e575e2fe092ec716d139dbc4621
Cedrik-Barutel <cedrik.barutel@gmail.com>
2026-01-06 16:32:03 +0100

    update

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-// intro animation
-gsap.from(".box", {
-  y: -200,
-  opacity: 0,
-  duration: 1.5,
-  rotation: 360,
-  ease: "bounce.out"
-  repeat:-1
-});
+const tl = gsap.timeline()
+.to("path", {attr:{
+  d:"M 100 100 C 100 100 150 212 250 100 C 350 -12 400 100 400 100"
+}})
+GSDevTools.create({animation:tl})
 
