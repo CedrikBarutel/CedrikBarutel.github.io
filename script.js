8cd729eb2862ae6eb5fb6d09e7e769b3a20bc590
Cedrik-Barutel <cedrik.barutel@gmail.com>
2026-01-06 12:52:38 +0100

    test

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-gsap.to(".box", { 
-  duration: 2,
-  x: 200,
-  rotation: 360,
+// intro animation
+gsap.from(".title", {
+  y: -50,
+  opacity: 0,
+  duration: 1,
+  ease: "power3.out"
+});
+
+gsap.from(".box", {
+  scale: 0,
+  rotation: 180,
+  duration: 1.2,
+  ease: "back.out(1.7)"
+});
+
+// button-triggered animation
+const box = document.querySelector(".box");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+  gsap.to(box, {
+    x: gsap.utils.random(-200, 200),
+    y: gsap.utils.random(-100, 100),
+    rotation: gsap.utils.random(0, 360),
+    borderRadius: gsap.utils.random(0, 50) + "%",
+    duration: 1,
+    ease: "elastic.out(1, 0.5)"
+  });
 });
 
